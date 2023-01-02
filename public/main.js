8b1b906098166d219e6b0eb3a3cfb9ab89b3a7ef
ajeddin <abduljeddin@gmail.com>
2023-01-02 06:13:32 +0300

    progress almost done

--- public/main.js
+++ public/main.js
@@ -10,20 +10,25 @@ const qouteCard = document.createElement('div')
 qouteCard.classList.add('qoute-card')
     
         qouteCard.innerHTML = `    
-        <div class="qoute-card">
-        <h2id='qouteDisplay'>${qoute.qoute}</h2>
+        <div id="qoute-${qoute['qoutes_id']}" class="qoute-card">
+        <h2 id="qoute">${qoute.qoute}</h2>
         <h3 id='author'>Author: ${qoute.author}</h3>
         <h3 id='emotion'>Qoute: ${qoute.emotion}</h3>
-        </div>
         <button onclick="deleteQoute(${qoute.qoutes_id})">delete</button>
+        </div>
         `
-    
+
     
         qouteSection.appendChild(qouteCard)})
     })
 }
 const deleteQoute = (id) => {
-    axios.delete(`http://localhost:8765/deleteQoute/${id}`).then(console.log('deleted'))
-    getAllQoutes()
+    axios.delete(`http://localhost:8765/deleteQoute/${id}`).then(()=> {
+        const qouteCard = document.getElementById(`qoute-${id}`)
+    
+        qouteCard.remove()
+    }
+    )
+  
 }
 getAllQoutes()
